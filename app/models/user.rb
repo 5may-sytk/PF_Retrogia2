3f3f582c40157867a92cc014e6c4deac9e155d53
5may-sytk <189351572 +5may-sytk@users.noreply.github.com>
2025-02-18 17:21:31 +0000

    モデル変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,7 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   #has_many :bookmarks, dependent: :destroy
 
   # フォローしているユーザーとのアクティブなリレーションシップ
