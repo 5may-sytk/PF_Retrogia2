459e3db38ad84174dc92313ac9c2bfb22aee72cf
5may-sytk <189351572 +5may-sytk@users.noreply.github.com>
2025-02-19 19:44:03 +0000

    ブックマーク機能実装

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -4,7 +4,7 @@ class Public::BookmarksController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @bookmark = current_user.bookmark.new(post_id: @post)
+    @bookmark = current_user.bookmarks.new(post_id: @post.id)
     @bookmark.save
     redirect_to request.referer
   end
@@ -15,7 +15,7 @@ class Public::BookmarksController < ApplicationController
 
   def destroy
     @post = Post.find(params[:post_id])
-    @bookmark = current_user.bookmarks.find_by(post: @post)
+    @bookmark = current_user.bookmarks.find_by(post_id: @post.id)
     @bookmark.destroy
     redirect_to request.referer
   end
