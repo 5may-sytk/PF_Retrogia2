6dc9bad1c2f2a54558066816d9c032f52b6908ad
5may-sytk <189351572 +5may-sytk@users.noreply.github.com>
2025-02-19 16:03:52 +0000

    Search変数忘れ

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -2,11 +2,11 @@ class Admin::SearchesController < ApplicationController
   def search
     @word = params[:word]
 
-    if params[:word].blank? && params[:is_status] == "1"
+    if @word.blank? && params[:is_status] == "1"
       @users = User.where(is_active: false)
-    elsif params[:word].blank? && params[:is_status] == "0"
+    elsif @word.blank? && params[:is_status] == "0"
       redirect_to admin_users_path
-    elsif params[:word] =~ /\A[a-zA-Z0-9]{10}\z/
+    elsif @word =~ /\A[a-zA-Z0-9]{10}\z/
       @users = User.where(unique_id: @word)
     else
       @users = User.where("name LIKE ?", "%#{@word}%")
