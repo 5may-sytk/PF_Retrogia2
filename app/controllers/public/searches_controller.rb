a949b9f4c537c2fa3661246e5aa3e09c985b9b7f
5may-sytk <189351572 +5may-sytk@users.noreply.github.com>
2025-02-20 09:04:29 +0000

    いったん非公開のものは見れないように設定

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -8,15 +8,21 @@ class Public::SearchesController < ApplicationController
     if @range != "ユーザー"
       @posts = Post.where("title LIKE ?", "%#{@word}%")
     end
-
+  
     if @word.blank?
       redirect_to public_posts_path
+      return
     end
   
     if @word.match(/\A[a-zA-Z0-9]{10}\z/)
       @users = User.where(unique_id: @word)
     else
       @users = User.where("name LIKE ?", "%#{@word}%")
+    end
+  
+    if @users.exists?(is_public: false)
+      redirect_to public_posts_path
+      return
     end
   end
 end
