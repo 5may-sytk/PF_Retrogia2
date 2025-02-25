cf6d435e4ad03a97525dfefb17def26fa1b418dd
5may-sytk <189351572 +5may-sytk@users.noreply.github.com>
2025-02-25 12:52:59 +0000

    3rdデプロイ+テスト完了

--- app/controllers/public/maps_controller.rb
+++ app/controllers/public/maps_controller.rb
@@ -1,4 +1,5 @@
 class Public::MapsController < ApplicationController
+  before_action :authenticate_user!
   def show
     @post = Post.find_by(id:params[:post_id])
     gon.post = Post.find_by(id:params[:post_id])
