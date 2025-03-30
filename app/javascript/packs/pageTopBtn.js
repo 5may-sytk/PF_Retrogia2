ec89076315650542ce32e037bf1c18a1dbe17f85
5may-sytk <189351572 +5may-sytk@users.noreply.github.com>
2025-03-30 06:59:46 +0000

    javascript修正

--- app/javascript/packs/pageTopBtn.js
+++ app/javascript/packs/pageTopBtn.js
@@ -1,8 +1,12 @@
-$(function() {
+// $(function() {
+// });
+
+$(document).on('turbolinks:load', function() {
   $('#back a').on('click',function(event){
     $('body, html').animate({
       scrollTop:0
     }, 800);
     event.preventDefault();
   });
+  // turbolinksを無効化したい処理
 });
